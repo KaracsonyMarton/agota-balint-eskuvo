a4959afdea4c0c219119fcf86cf97a1e85e706e4
Karácsony Márton <41687291+KaracsonyMarton@users.noreply.github.com>
2023-01-14 14:31:38 +0100

    [update] init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <!-- Meta Tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Page Title -->
+    <title> Ágota & Bálint 2023.06.10</title>
+
+    <!-- Favicon and Touch Icons -->
+    <link href="{{asset('images/favicon/favicon.png')}}" rel="shortcut icon" type="image/png">
+    <link href="{{asset('images/favicon/apple-touch-icon.png')}}" rel="apple-touch-icon">
+    <link href="{{asset('images/favicon/apple-touch-icon-72x72.png')}}" rel="apple-touch-icon" sizes="72x72">
+    <link href="{{asset('images/favicon/apple-touch-icon-114x114.png')}}" rel="apple-touch-icon" sizes="114x114">
+    <link href="{{asset('images/favicon/apple-touch-icon-144x144.png')}}" rel="apple-touch-icon" sizes="144x144">
+
+    <!-- Icon fonts -->
+    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/flaticon.css')}}" rel="stylesheet">
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- Plugins for this template -->
+    <link href="{{asset('css/animate.css')}}" rel="stylesheet">
+    <link href="{{asset('css/owl.carousel.css')}}" rel="stylesheet">
+    <link href="{{asset('css/owl.theme.css')}}" rel="stylesheet">
+    <link href="{{asset('css/slick.css')}}" rel="stylesheet">
+    <link href="{{asset('css/slick-theme.css')}}" rel="stylesheet">
+    <link href="{{asset('css/owl.transitions.css')}}" rel="stylesheet">
+    <link href="{{asset('css/jquery.fancybox.css')}}" rel="stylesheet">
+    <link href="{{asset('css/magnific-popup.css')}}" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="home">
+
+<!-- start page-wrapper -->
+<div class="page-wrapper">
+
+    <!-- start preloader -->
+    <div class="preloader">
+        <div class="inner">
+            <span class="icon"><i class="fi flaticon-two"></i></span>
+        </div>
+    </div>
+    <!-- end preloader -->
+
+    <!-- Start header -->
+    <header id="header" class="site-header header-style-2">
+        <nav class="navigation navbar navbar-default">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="open-btn">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <div class="couple-logo">
+                        <h1><a href="#home">Á <i class="fi flaticon-shape-1"></i> B</a></h1>
+                    </div>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse navbar-right navigation-holder">
+                    <button class="close-navbar"><i class="fa fa-close"></i></button>
+                    <ul class="nav navbar-nav">
+                        <li><a href="#home">Főoldal</a></li>
+                        <li><a href="#events">Események</a></li>
+                        <li><a href="#rsvp">RSVP</a></li>
+                    </ul>
+                </div><!-- end of nav-collapse -->
+            </div><!-- end of container -->
+        </nav>
+    </header>
+    <!-- end of header -->
+
+
+    <!-- start of hero -->
+    <section class="hero">
+        <div class="static-hero"></div>
+        <div id="spirit-header" class="spirit-header">
+            <canvas id="spirit-canvas"></canvas>
+        </div>
+
+        <div class="wedding-announcement">
+            <div class="couple-name-merried-text">
+                <h2 class="wow slideInUp" data-wow-duration="1s">Ágota &amp; Bálint</h2>
+                <div class="married-text wow fadeIn" data-wow-delay="1s">
+                    <h4 class="">
+                        <span class=" wow fadeInUp" data-wow-delay="1.05s">W</span>
+                        <span class=" wow fadeInUp" data-wow-delay="1.10s">e</span>
+                        <span class=" wow fadeInUp" data-wow-delay="1.15s">'</span>
+                        <span class=" wow fadeInUp" data-wow-delay="1.20s">r</span>
+                        <span class=" wow fadeInUp" data-wow-delay="1.25s">e</span>
+                        <span>&nbsp;</span>
+                        <span class=" wow fadeInUp" data-wow-delay="1.30s">g</span>
+                        <span class=" wow fadeInUp" data-wow-delay="1.35s">e</span>
+                        <span class=" wow fadeInUp" data-wow-delay="1.40s">t</span>
+                        <span class=" wow fadeInUp" data-wow-delay="1.45s">t</span>
+                        <span class=" wow fadeInUp" data-wow-delay="1.50s">i</span>
+                        <span class=" wow fadeInUp" data-wow-delay="1.55s">n</span>
+                        <span class=" wow fadeInUp" data-wow-delay="1.60s">g</span>
+                        <span>&nbsp;</span>
+                        <span class=" wow fadeInUp" data-wow-delay="1.65s">m</span>
+                        <span class=" wow fadeInUp" data-wow-delay="1.70s">a</span>
+                        <span class=" wow fadeInUp" data-wow-delay="1.75s">r</span>
+                        <span class=" wow fadeInUp" data-wow-delay="1.80s">r</span>
+                        <span class=" wow fadeInUp" data-wow-delay="1.85s">i</span>
+                        <span class=" wow fadeInUp" data-wow-delay="1.90s">e</span>
+                        <span class=" wow fadeInUp" data-wow-delay="1.95s">d</span>
+
+                    </h4>
+                </div>
+                <!-- <i class="fa fa-heart"></i> -->
+            </div>
+
+            <div class="save-the-date">
+                <h4>Mentsd el a dátumot</h4>
+                <span class="date">2023.06.10</span>
+            </div>
+        </div>
+    </section>
+    <!-- end of hero slider -->
+
+    <!-- start getting-there-section -->
+    <section class="getting-there-section section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col col-xs-12">
+                    <div class="section-title-white">
+                        <div class="vertical-line"><span><i class="fi flaticon-two"></i></span></div>
+                        <h2>Megközelítés</h2>
+                    </div>
+                </div>
+            </div> <!-- end section-title -->
+
+            <div class="row content">
+                <div class="col col-md-6">
+                    <h3>Közlekedés</h3>
+                    <p> industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
+                        type and scrambled it to make a type specimen book. It has survived not only five centuries, but
+                        also the leap into electronic typesetting, remaining essentially unchanged. It was popularised
+                        in the 1960s</p>
+                </div>
+                <div class="col col-md-6">
+                    <h3>Szállás</h3>
+                    <p> industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
+                        type and scrambled it to make a type specimen book. It has survived not only five centuries, but
+                        also the leap into electronic typesetting, remaining essentially unchanged. It was popularised
+                        in the 1960s</p>
+                </div>
+            </div> <!-- end row -->
+        </div> <!-- end container -->
+    </section>
+    <!-- end getting-there-section -->
+    <!-- start count-down-section -->
+    <section class="count-down-section section-padding parallax" data-bg-image="images/countdown-bg.jpg" data-speed="7">
+        <div class="container">
+            <div class="row">
+                <div class="col col-md-4">
+                    <h2><span>We are waiting for.....</span> The adventure</h2>
+                </div>
+                <div class="col col-md-7 col-md-offset-1">
+                    <div class="count-down-clock">
+                        <div id="clock">
+
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- end row -->
+        </div> <!-- end container -->
+    </section>
+    <!-- end count-down-section -->
+
+    <!-- start events-section -->
+    <section class="events-section section-padding" id="events">
+        <div class="container">
+            <div class="row">
+                <div class="col col-xs-12">
+                    <div class="section-title">
+                        <div class="vertical-line"><span><i class="fi flaticon-two"></i></span></div>
+                        <h2>Wedding events</h2>
+                    </div>
+                </div>
+            </div> <!-- end section-title -->
+
+            <div class="row">
+                <div class="col col-lg-10 col-lg-offset-1">
+                    <div class="event">
+                        <div class="img-holder">
+                            <img src="images/events/img-1.jpg" alt class="img img-responsive">
+                        </div>
+                        <div class="details">
+                            <h3>The reception</h3>
+                            <ul>
+                                <li><i class="fa fa-map-marker"></i> 32 big bro road, Chanpai, London.</li>
+                                <li><i class="fa fa-clock-o"></i> Nov 25 2017, 9AM - 5PM</li>
+                            </ul>
+                            <p>when an unknown printer took a galley of type and scrambled it to make a type specimen
+                                book. It has survived not only five centuries, but also the leap into electronic
+                                typesetting, remaining essentially unchanged. </p>
+                            <a class="see-location-btn popup-gmaps"
+                               href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25211.21212385712!2d144.95275648773628!3d-37.82748510398018!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0x5045675218ce7e0!2zTWVsYm91cm5lIFZJQyAzMDA0LCDgpoXgprjgp43gpp_gp43gprDgp4fgprLgpr_gpq_gprzgpr4!5e0!3m2!1sbn!2sbd!4v1503742051881">
+                                See location <i class="fa fa-angle-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="event">
+                        <div class="img-holder">
+                            <img src="images/events/img-2.jpg" alt class="img img-responsive">
+                        </div>
+                        <div class="details">
+                            <h3>Wedding party</h3>
+                            <ul>
+                                <li><i class="fa fa-map-marker"></i> 32 big bro road, Chanpai, London.</li>
+                                <li><i class="fa fa-clock-o"></i> Nov 25 2017, 9AM - 5PM</li>
+                            </ul>
+                            <p>when an unknown printer took a galley of type and scrambled it to make a type specimen
+                                book. It has survived not only five centuries, but also the leap into electronic
+                                typesetting, remaining essentially unchanged. </p>
+                            <a class="see-location-btn popup-gmaps"
+                               href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3152.0160484383277!2d144.99053291585201!3d-37.81309307975254!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642ef89a7e023%3A0xb1353055e38c1ab8!2sNew+York+Tomato+Cafe!5e0!3m2!1sbn!2sbd!4v1503743893919">
+                                See location <i class="fa fa-angle-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- end row -->
+        </div> <!-- end container -->
+    </section>
+    <!-- end events-section -->
+
+    <!-- start rsvp-section -->
+    <section class="rsvp-section section-padding parallax" data-bg-image="images/rsvp-bg.jpg" data-speed="7" id="rsvp">
+        <div class="container">
+            <div class="row">
+                <div class="col col-xs-12">
+                    <div class="section-title-white">
+                        <div class="vertical-line"><span><i class="fi flaticon-two"></i></span></div>
+                        <h2>Join our party</h2>
+                    </div>
+                </div>
+            </div> <!-- end section-title -->
+
+            <div class="row content">
+                <div class="col col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <form id="rsvp-form" class="form validate-rsvp-form row" method="post">
+                        <div class="col col-sm-6">
+                            <input type="text" name="name" class="form-control" required placeholder="Név*">
+                        </div>
+                        <div class="col col-sm-6">
+                            <input type="text" name="name" class="form-control" placeholder="+1 fő">
+                        </div>
+                        <div class="col col-sm-6">
+                            <select class="form-control" name="attending">
+                                <option selected>Jövök</option>
+                                <option>Nem jövök</option>
+                            </select>
+                        </div>
+                        <div class="col col-sm-12">
+                            <textarea class="form-control" name="notes" placeholder="Étel allergiát kérlek itt tüntesd fel"></textarea>
+                        </div>
+                        <div class="col col-sm-12 submit-btn">
+                            <button type="submit" class="submit">Küldés</button>
+                            <span id="loader"><i class="fa fa-refresh fa-spin fa-3x fa-fw"></i></span>
+                        </div>
+                    </form>
+                </div>
+            </div> <!-- end row -->
+        </div> <!-- end container -->
+    </section>
+    <!-- end rsvp-section -->
+
+
+</div>
+<!-- end of page-wrapper -->
+
+
+<!-- All JavaScript files
+================================================== -->
+<script src="{{ asset('js/jquery.min.js')}}"></script>
+<script src="{{ asset('js/bootstrap.min.js')}}"></script>
+
+<!-- Plugins for this template -->
+<script src="{{ asset('js/jquery-plugin-collection.js')}}"></script>
+<script src="{{ asset('js/spirit.js')}}"></script>
+
+<!-- Custom script for this template -->
+<script src="{{ asset('js/script.js')}}"></script>
+</body>
+</html>
