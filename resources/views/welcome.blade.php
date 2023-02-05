6b0a9a64068a04efca260d5790c1593382a659d7
Karácsony Márton <41687291+KaracsonyMarton@users.noreply.github.com>
2023-02-05 08:33:53 +0100

    [update] more updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Page Title -->
-    <title> Ágota & Bálint 2023.06.10</title>
+    <title> ÁGOTA & BÁLINT 2023.06.10</title>
 
     <!-- Favicon and Touch Icons -->
     <link href="{{asset('images/favicon/favicon.png')}}" rel="shortcut icon" type="image/png">
@@ -78,7 +78,7 @@
                         <span class="icon-bar"></span>
                     </button>
                     <div class="couple-logo">
-                        <h1><a href="#home">Ágota és Bálint</a></h1>
+                        <h1><a href="#home">ÁGOTA & BÁLINT</a></h1>
                     </div>
                 </div>
                 <div id="navbar" class="navbar-collapse collapse navbar-right navigation-holder">
@@ -86,7 +86,7 @@
                     <ul class="nav navbar-nav">
                         <li><a href="#home">Főoldal</a></li>
                         <li><a href="#events">Események</a></li>
-                        <li><a href="#rsvp">RSVP</a></li>
+                        <li><a href="#rsvp">Visszajelzés</a></li>
                     </ul>
                 </div><!-- end of nav-collapse -->
             </div><!-- end of container -->
@@ -104,7 +104,7 @@
 
         <div class="wedding-announcement">
             <div class="couple-name-merried-text">
-                <h2 class="wow slideInUp" data-wow-duration="1s">Ágota &amp; Bálint</h2>
+                <h2 class="wow slideInUp" data-wow-duration="1s">ÁGOTA & BÁLINT</h2>
                 <div class="married-text wow fadeIn" data-wow-delay="1s">
                     <h4 class="">
                         <span class=" wow fadeInUp" data-wow-delay="1.05s">ü</span>
@@ -158,7 +158,7 @@
                 <div class="col col-xs-12">
                     <div class="section-title-white">
                         <div class="vertical-line"><span><i class="fi flaticon-two"></i></span></div>
-                        <h2>Megközelítés</h2>
+                        <h2>Helyszín</h2>
                         <h4 class="white">Gyarmatpusztai Vadászház (Gyermely, Gyarmatpuszta 9, 2821)</h4>
                         <a class=" btn btn-success" href="https://goo.gl/maps/VVLjiRJS4X4NwYmH6" target="_blank">Útvonalterv</a>
                     </div>
@@ -167,15 +167,12 @@
 
             <div class="row content">
                 <div class="col col-md-6">
-                    <h3>Közlekedés</h3>
+                    <h3>Megközelítés</h3>
                     <p>Budapesttől 45 km, autóval 45 perc </p>
                 </div>
                 <div class="col col-md-6">
                     <h3>Szállás</h3>
-                    <p> A Vadászházban <b>(13.000,-Ft/fő/éj)</b> és ha többen kértek szállást, a Palota Panzióban <b>(8000,
-                            - Ft / fő / éj reggelivel)</b> Gyermelyen.<br> A Vadászháztól a Palota Panzióig gondoskodunk
-                        a
-                        fuvarról.</p>
+                    <p>Létszámtól függően a Vadászházban vagy a Palota Panzióban.</p>
                 </div>
             </div> <!-- end row -->
         </div> <!-- end container -->
@@ -207,7 +204,7 @@
                 <div class="col col-xs-12">
                     <div class="section-title">
                         <div class="vertical-line"><span><i class="fi flaticon-two"></i></span></div>
-                        <h2>Wedding events</h2>
+                        <h2>Események</h2>
                     </div>
                 </div>
             </div> <!-- end section-title -->
@@ -215,9 +212,6 @@
             <div class="row">
                 <div class="col col-lg-10 col-lg-offset-1">
                     <div class="event">
-                        <div class="img-holder">
-                            <img src="{{asset('images/events/img-1.jpg')}}" alt class="img img-responsive">
-                        </div>
                         <div class="details">
                             <h3>Érkezés</h3>
                             <ul>
@@ -229,86 +223,54 @@
                         </div>
                     </div>
                     <div class="event">
-                        <div class="img-holder">
-                            <img src="{{asset('images/events/img-2.jpg')}}" alt class="img img-responsive">
-                        </div>
                         <div class="details">
                             <h3>Szertartás</h3>
                             <ul>
-                                <li><i class="fa fa-map-marker"></i>Gyarmatpusztai Vadászház (Gyermely, Gyarmatpuszta 9,
-                                    2821)
-                                </li>
                                 <li><i class="fa fa-clock-o"></i> 17:30</li>
                             </ul>
                         </div>
                     </div>
                     <div class="event">
-                        <div class="img-holder">
-                            <img src="{{asset('images/events/img-2.jpg')}}" alt class="img img-responsive">
-                        </div>
+
                         <div class="details">
                             <h3>Gratuláció</h3>
                             <ul>
-                                <li><i class="fa fa-map-marker"></i>Gyarmatpusztai Vadászház (Gyermely, Gyarmatpuszta 9,
-                                    2821)
-                                </li>
                                 <li><i class="fa fa-clock-o"></i> 18:00</li>
                             </ul>
                         </div>
                     </div>
                     <div class="event">
-                        <div class="img-holder">
-                            <img src="{{asset('images/events/img-2.jpg')}}" alt class="img img-responsive">
-                        </div>
                         <div class="details">
                             <h3>Fotózás</h3>
                             <ul>
-                                <li><i class="fa fa-map-marker"></i>Gyarmatpusztai Vadászház (Gyermely, Gyarmatpuszta 9,
-                                    2821)
-                                </li>
                                 <li><i class="fa fa-clock-o"></i> 18:30-19:00</li>
                             </ul>
                         </div>
                     </div>
                     <div class="event">
-                        <div class="img-holder">
-                            <img src="{{asset('images/events/img-2.jpg')}}" alt class="img img-responsive">
-                        </div>
                         <div class="details">
                             <h3>Vacsora</h3>
                             <ul>
-                                <li><i class="fa fa-map-marker"></i>Gyarmatpusztai Vadászház (Gyermely, Gyarmatpuszta 9,
-                                    2821)
-                                </li>
                                 <li><i class="fa fa-clock-o"></i> 20:00</li>
                             </ul>
                         </div>
                     </div>
                     <div class="event">
-                        <div class="img-holder">
-                            <img src="{{asset('images/events/img-2.jpg')}}" alt class="img img-responsive">
-                        </div>
                         <div class="details">
                             <h3>Buli</h3>
                             <ul>
-                                <li><i class="fa fa-map-marker"></i>Gyarmatpusztai Vadászház (Gyermely, Gyarmatpuszta 9,
-                                    2821)
-                                </li>
                                 <li><i class="fa fa-clock-o"></i> Hajnalig</li>
                             </ul>
                         </div>
                     </div>
                     <div class="event">
-                        <div class="img-holder">
-                            <img src="{{asset('images/events/img-2.jpg')}}" alt class="img img-responsive">
-                        </div>
+{{--                        <div class="img-holder">--}}
+{{--                            <img src="{{asset('images/events/img-2.jpg')}}" alt class="img img-responsive">--}}
+{{--                        </div>--}}
                         <div class="details">
                             <h3>Morzsaparti</h3>
                             <ul>
-                                <li><i class="fa fa-map-marker"></i>Gyarmatpusztai Vadászház (Gyermely, Gyarmatpuszta 9,
-                                    2821)
-                                </li>
-                                <li><i class="fa fa-clock-o"></i> 17:30</li>
+                                <li><i class="fa fa-clock-o"></i> Másnap reggel</li>
                             </ul>
                         </div>
                     </div>
@@ -320,13 +282,13 @@
     <!-- end events-section -->
 
     <!-- start rsvp-section -->
-    <section class="rsvp-section section-padding parallax" data-bg-image="images/rsvp-bg.jpg" data-speed="7" id="rsvp">
+    <section class="rsvp-section section-padding parallax" data-bg-image="images/back.jpg" data-speed="7" id="rsvp" style="">
         <div class="container">
             <div class="row">
                 <div class="col col-xs-12">
                     <div class="section-title-white">
                         <div class="vertical-line"><span><i class="fi flaticon-two"></i></span></div>
-                        <h2>Répondez s'il vous plaît</h2>
+                        <h2>Visszajelzés</h2>
                     </div>
                 </div>
             </div> <!-- end section-title -->
@@ -336,13 +298,13 @@
                     <form class="form validate-rsvp-form row" action="{{route('sendEmail')}}"
                           method="post">
                         @csrf
-                        <div class="col col-sm-6">
+                        <div class="col col-sm-12">
                             <input type="text" name="name" class="form-control" required placeholder="Név*">
                         </div>
-                        <div class="col col-sm-6">
-                            <input type="text" name="additional_person_name" class="form-control"
-                                   placeholder="Kivel jössz? Kérlek írd le a nevét.">
-                        </div>
+{{--                        <div class="col col-sm-6">--}}
+{{--                            <input type="text" name="additional_person_name" class="form-control"--}}
+{{--                                   placeholder="Kivel jössz? Kérlek írd le a nevét.">--}}
+{{--                        </div>--}}
                         <div class="col col-sm-12">
                             <select class="form-control" name="attending">
                                 <option selected>Ott leszek</option>
@@ -350,11 +312,10 @@
                             </select>
                         </div>
                         <div class="col col-sm-12">
-                            <label class="white" for="accommodation">Kérsz, kértek szállást? <small>(Vadászházban: 13.000, - Ft / fő / éj,
-                                Palota Panzióban: 8000, - Ft / fő / éj reggelivel) Gyermelyen)</small> </label>
+                            <label class="white" for="accommodation">Szállás  <small>(~13.000, - Ft / fő / éj)</small> </label>
                             <select class="form-control" id="accommodation" name="accommodation">
-                                <option selected>Igen</option>
-                                <option>Nem</option>
+                                <option selected>Kérek</option>
+                                <option>Nem kérek</option>
                             </select>
                         </div>
                         <div class="col col-sm-12">
